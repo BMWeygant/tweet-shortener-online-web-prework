0be5692a82cc275f4dd90a5ea6e66b45d8de3dca
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-16 19:55:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def dictionary
+  shorthand = {
+     "hello" => "hi",
+     "to" => "2",
+     "two" => "2",
+     "too" => "2",
+     "for" => "4",
+     "four" => "4",
+     "be" => "b",
+     "you" => "u",
+     "at" => "@",
+     "and" => "&"
+  }
+  end
+
+  def word_substituter(string)
+    string.split.collect do |word|
+      if dictionary.keys.include?(word)
+        word = dictionary[word]
+      else
+        word
+      end
+    end.join(" ")
+  end
